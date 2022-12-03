6367e2bd8ea6cb37584de9dec06ed954128abe67
D-Dursty <devondahlia@gmail.com>
2022-12-03 14:59:35 -0800

    another route update

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,31 +2,47 @@ const router = require('express').Router();
 const { User, Blog, Comment } = require('../../models');
 
 
-router.get('/', (req, res) => {
-  Comment.findAll().then(comments=>{
-    const commentsHbsData = comments.map((comment)=>comment.get({plain: true}))
-    console.log(comments);
-    console.log(commentsHbsData)
-    res.render("singlepost", {comments:commentsHbsData, logged_in: req.session.logged_in
-    })
-  })
-})
-    
-router.post('/', async (req, res) => {
-  if(!req.session.logged_in){
-    return res.status(401).json({msg:"You must login to do that :)"})
-    }  
-  try {
-    const newComment = await Comment.create({
-      content:req.body.content,
-      date_created:req.body.date_created,
-      post_id:req.body.post_id,
-      user_id:req.session.user_id
-    });
-    res.status(200).json(newComment);
-  } catch (err) {
-    res.status(400).json(err);
-  }
+router.get('/', async(req,res)=>{
+    try{
+        const commentData = await Comment.findAll();
+        res.json(commentData);
+    }catch(err){
+        res.json({message:err.message})
+    }
 });
+
+router.get('/:id', async(req,res)=>{
+    try{
+        const commentsData = await Comment.findByPk(req.params.id);
+        res.json(commentsData);
+    }catch(err){
+        res.json({message:err.message})
+    }
+})
+
+router.post('/:id', async(req,res)=>{
+    if (!req.session.activeUser){
+        return res.status(401).json({message:"Must be logged in to comment."})
+    }
+    try{
+        const newcommentData = await Comment.create({
+            body:req.body.body,
+            UserId: req.session.activeUser.id,
+            PostId: req.params.id
+        });
+        res.json(newcommentData);
+    }catch(err){
+        res.json({message:err.message})
+    }
+})
+
+router.delete('/:id', async(req,res)=>{
+    try{
+        const delData = await Comment.destroy({where:{id:req.params.id}})
+        res.json(delData)
+    }catch(err){
+        res.json({message:err.message})
+    }
+})
 
 module.exports = router;
---
+++ controllers/frontEndRoutes.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+const { User, Post, Comment } = require('../models');
+const { route } = require('./api');
+const sequelize = require('sequelize');
+
+router.get("/", (req, res) => {
+    Post.findAll().then(posts => {
+        const postsHbsData = posts.map(post => post.get({ plain: true }))
+        console.log(posts);
+        res.render("home", {
+            posts: postsHbsData,
+            logged_in: req.session.logged_in
+        })
+    })
+
+})
+
+router.get('/post/:id', (req, res) => {
+    Post.findByPk(req.params.id, {
+        include: [User,
+            {
+                model: Comment,
+                include: User,
+            },
+        ],
+    }).then(post => {
+        const postHbsData = post.get({ plain: true });
+        console.log(post);
+        console.log(postHbsData)
+        postHbsData.logged_in = req.session.logged_in
+        res.render("singlepost", postHbsData)
+    })
+})
+
+router.get("/sessions", (req, res) => {
+    res.json(req.session)
+});
+
+router.get("/login", (req, res) => {
+    if (req.session.logged_in) {
+        return res.redirect("/dashboard")
+    }
+    res.render("login")
+});
+
+router.get("/dashboard", (req, res) => {
+    if (!req.session.logged_in) {
+        return res.redirect("/login")
+    }
+    User.findByPk(req.session.user_id, {
+        include: [Post]
+    }).then(userData => {
+        const hbsData = userData.toJSON();
+        console.log(hbsData)
+        hbsData.logged_in = req.session.logged_in
+        res.render("dashboard", hbsData)
+    })
+})
+
+module.exports = router;
